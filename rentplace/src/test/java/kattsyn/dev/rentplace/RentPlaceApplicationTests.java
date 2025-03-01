15adca82bc370dc3b0969c965f5b16ea33ddc30e
Kattsyn Vladimir Yurievich <78604784+Kattsyn@users.noreply.github.com>
2025-03-01 11:47:44 +0300

    init commit

---
+++ rentplace/src/test/java/kattsyn/dev/rentplace/RentPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kattsyn.dev.rentplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
