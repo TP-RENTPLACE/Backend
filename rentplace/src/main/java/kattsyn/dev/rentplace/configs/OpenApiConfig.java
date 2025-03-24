c55995902f82edac412eb30174844017d09118b0
Kattsyn <vov4ikkat3@mail.ru>
2025-03-25 02:27:36 +0300

    added: dependencies for swagger and OpenApiConfig.java

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/configs/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package kattsyn.dev.rentplace.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SocialNetwork API",
+                description = "SocialNetwork", version = "1.0.0",
+                contact = @Contact(
+                        name = "Kattsyn Vladimir",
+                        email = "warshard1337@gmail.com"
+                )
+        )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+
+public class OpenApiConfig {
+}
