b053815bbce3ceeed1383c7df7c64c8122c1adc7
Kattsyn Vladimir Yurievich <78604784+Kattsyn@users.noreply.github.com>
2025-03-29 12:20:39 +0300

    (TP-59) feature: add categoryDTO

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/dtos/CategoryDTO.java
@@ -0,0 +1,14 @@
+package kattsyn.dev.rentplace.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO {
+
+    private String name;
+
+}
