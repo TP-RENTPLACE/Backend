5f26aeab0bf5ba26079b83bb35c0e766dbae6fb7
Kattsyn Vladimir Yurievich <78604784+Kattsyn@users.noreply.github.com>
2025-03-29 12:20:26 +0300

    (TP-59) feature: add category entity

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/entities/Category.java
@@ -0,0 +1,26 @@
+package kattsyn.dev.rentplace.entities;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private long categoryId;
+    @Column(name = "name", nullable = false)
+    @Schema(description = "Название категории")
+    private String name;
+
+}
