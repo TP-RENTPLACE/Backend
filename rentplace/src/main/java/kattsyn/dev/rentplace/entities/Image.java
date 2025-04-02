922ace7ad4ee8c5b542d9444c4058700e70208a3
Kattsyn <vov4ikkat3@mail.ru>
2025-04-03 02:55:26 +0300

    (TP-71) feat: add image entity

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/entities/Image.java
@@ -0,0 +1,31 @@
+package kattsyn.dev.rentplace.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private long imageId;
+
+    @Column(name = "file_name")
+    private String fileName;
+    @Column(name = "original_file_name")
+    private String originalFileName;
+    @Column(name = "content_type")
+    private String contentType;
+    @Column(name = "size")
+    private long size;
+    @Column(name = "is_preview_image")
+    private boolean isPreviewImage;
+
+}
