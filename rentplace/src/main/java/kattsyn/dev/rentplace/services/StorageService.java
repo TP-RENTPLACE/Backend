4bda5130b565fcc3e9dd94620a56e82dcd73c86d
Kattsyn <vov4ikkat3@mail.ru>
2025-04-03 02:56:38 +0300

    (TP-71) feat: add store service interface

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/services/StorageService.java
@@ -0,0 +1,11 @@
+package kattsyn.dev.rentplace.services;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    String store(MultipartFile file);
+    Resource loadAsResource(String filename);
+    void delete(String filename);
+}
