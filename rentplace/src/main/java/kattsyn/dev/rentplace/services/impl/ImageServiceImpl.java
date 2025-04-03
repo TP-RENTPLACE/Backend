2a3137cd0154d29703cfe09627aeda9b2147e85b
Kattsyn <vov4ikkat3@mail.ru>
2025-04-03 18:13:57 +0300

    (TP-71) feat: change image service interface and impl add file validation changed methods

--- rentplace/src/main/java/kattsyn/dev/rentplace/services/impl/ImageServiceImpl.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/services/impl/ImageServiceImpl.java
@@ -2,6 +2,7 @@ package kattsyn.dev.rentplace.services.impl;
 
 import jakarta.transaction.Transactional;
 import kattsyn.dev.rentplace.entities.Image;
+import kattsyn.dev.rentplace.enums.ImageType;
 import kattsyn.dev.rentplace.repositories.ImageRepository;
 import kattsyn.dev.rentplace.services.ImageService;
 import kattsyn.dev.rentplace.services.StorageService;
@@ -17,12 +18,25 @@ public class ImageServiceImpl implements ImageService {
     private final ImageRepository imageRepository;
     private final StorageService storageService;
 
+    private void validateImage(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (file.isEmpty()) {
+            throw new RuntimeException("Файл пустой");
+        }
+        if (!"image/jpeg".equals(contentType) && !"image/png".equals(contentType)) {
+            throw new RuntimeException("Поддерживаются только JPEG и PNG изображения");
+        }
+        if (file.getSize() > 5 * 1024 * 1024) {
+            throw new RuntimeException("Файл слишком большой");
+        }
+    }
+
     @Transactional
     @Override
     public Image uploadImage(MultipartFile file) {
-        //todo: добавить проверку на вес файла и contentType
-        String filename = storageService.store(file);
+        validateImage(file);
 
+        String filename = storageService.store(file);
         Image image = new Image();
         image.setFileName(filename);
         image.setOriginalFileName(file.getOriginalFilename());
@@ -30,6 +44,48 @@ public class ImageServiceImpl implements ImageService {
         image.setSize(file.getSize());
 
         return imageRepository.save(image);
+    }
+
+    @Override
+    public Image uploadImage(MultipartFile file, ImageType type) {
+        try {
+            validateImage(file);
+
+            String filename = storageService.store(file, type.additionalPath);
+
+            Image image = new Image();
+            image.setFileName(filename);
+            image.setOriginalFileName(file.getOriginalFilename());
+            image.setContentType(file.getContentType());
+            image.setAdditionalPath(type.additionalPath);
+            image.setSize(file.getSize());
+
+            return imageRepository.save(image);
+        } catch (Exception e) {
+            storageService.rollbackUpload(type.additionalPath, file.getOriginalFilename());
+            throw e;
+        }
+    }
+
+    @Override
+    public Image uploadImage(MultipartFile file, String relativePath) {
+        try {
+            validateImage(file);
+
+            String filename = storageService.store(file, relativePath);
+
+            Image image = new Image();
+            image.setFileName(filename);
+            image.setOriginalFileName(file.getOriginalFilename());
+            image.setContentType(file.getContentType());
+            image.setAdditionalPath(relativePath);
+            image.setSize(file.getSize());
+
+            return imageRepository.save(image);
+        } catch (Exception e) {
+            storageService.rollbackUpload(relativePath, file.getOriginalFilename());
+            throw e;
+        }
     }
 
     @Transactional
@@ -42,7 +98,7 @@ public class ImageServiceImpl implements ImageService {
     @Transactional
     @Override
     public Resource getImageResource(Image image) {
-        return storageService.loadAsResource(image.getFileName());
+        return storageService.loadAsResource(image.getFileName(), image.getAdditionalPath());
     }
 
     @Transactional
@@ -51,7 +107,7 @@ public class ImageServiceImpl implements ImageService {
         Image image = imageRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Image not found"));
 
-        storageService.delete(image.getFileName());
+        storageService.delete(image.getAdditionalPath(), image.getFileName());
         imageRepository.delete(image);
     }
 
