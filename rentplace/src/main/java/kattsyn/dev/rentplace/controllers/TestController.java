56a8e06b52c78e61175515db985f3a6a93404542
Kattsyn <vov4ikkat3@mail.ru>
2025-04-12 16:20:02 +0300

    (TP-54) feat: commit to test ci-cd

--- rentplace/src/main/java/kattsyn/dev/rentplace/controllers/TestController.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/controllers/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping("/")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Hello World!!");
+        return ResponseEntity.ok("Hello World!!!");
     }
 
 }
