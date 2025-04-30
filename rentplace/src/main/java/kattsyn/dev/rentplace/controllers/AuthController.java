44df60e1c52c7842e376fc317b962e02f9126216
Kattsyn <vov4ikkat3@mail.ru>
2025-04-30 14:23:20 +0300

    (TP-77) feat: add AuthController

---
+++ rentplace/src/main/java/kattsyn/dev/rentplace/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package kattsyn.dev.rentplace.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.security.auth.message.AuthException;
+import kattsyn.dev.rentplace.dtos.JwtRequest;
+import kattsyn.dev.rentplace.dtos.JwtResponse;
+import kattsyn.dev.rentplace.dtos.RefreshJwtRequest;
+import kattsyn.dev.rentplace.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.path}/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация", description = "Для аутентификации, регистрации, обновлении и запроса токенов и отправки запросов кода на почту.")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest authRequest) throws AuthException {
+        final JwtResponse token = authService.login(authRequest);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) {
+        final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
+        final JwtResponse token = authService.refresh(request.getRefreshToken());
+        return ResponseEntity.ok(token);
+    }
+
+}
