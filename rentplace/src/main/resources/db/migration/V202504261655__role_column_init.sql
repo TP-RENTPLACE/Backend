beb0153dfe84cbab93e09d130d9e802e74c5f8e7
Kattsyn <vov4ikkat3@mail.ru>
2025-05-01 13:07:51 +0300

    (TP-77) feat: add new admin

--- rentplace/src/main/resources/db/migration/V202504261655__role_column_init.sql
+++ rentplace/src/main/resources/db/migration/V202504261655__role_column_init.sql
@@ -2,4 +2,5 @@ ALTER TABLE users
     ADD role VARCHAR(255) NOT NULL default 'ROLE_USER';
 
 INSERT INTO users (name, surname, email, role)
-VALUES ('admin', 'admin', 'warshard1337@gmail.com', 'ROLE_ADMIN');
+VALUES ('admin', 'admin', 'warshard1337@gmail.com', 'ROLE_ADMIN'),
+       ('Георгий', 'Бог войны', 'zagorodnew.gosha@gmail.com', 'ROLE_ADMIN');
