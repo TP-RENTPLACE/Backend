c47921eba3b623bf418f4d67e08afd5c978a24c3
Kattsyn <vov4ikkat3@mail.ru>
2025-05-04 02:09:51 +0300

    (TP-77) feat: add fields examples

--- rentplace/src/main/java/kattsyn/dev/rentplace/dtos/JwtResponse.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/dtos/JwtResponse.java
@@ -1,6 +1,6 @@
 package kattsyn.dev.rentplace.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +12,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class JwtResponse {
 
+    @Schema(example = "Bearer ")
     private final String type = "Bearer ";
+    @Schema(example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3YXJzaGFyZDEzMzdAZ21haWwuY29tIiwiZXhwIjoxNzQ2MzA1ODEzLCJyb2xlIjoiUk9MRV9B211JTiIsIm5hbWUiOiJhZG1pbiJ9.4Rg7E39Y4baT9Eld_pkvH0D6S72eepmyd17Ch44K5Fikw32BSbXsnVq4EOnXJgXsQkmkhZrGDHZh-cSGg7pLPg")
     private String accessToken;
 
     //@JsonIgnore
+    @Schema(example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3YXJzaGFyZDEzMzdAZ21haWwuY29tIiwiZXhwIjoxNzQ2MzA1ODEzLCJyb2xlIjoiUk9MRV9Bda1JTiIsIm5hbWUiOiJhZG1pbiJ9.4Rg7E39Y4baT9Eld_pkvH0D6S72eepmydCLCh44K5FikwkdBSbXsnVq4EOnXJgXsQkmkhZrGDHZh-cSGg7pLPg")
     private String refreshToken;
 
 }
