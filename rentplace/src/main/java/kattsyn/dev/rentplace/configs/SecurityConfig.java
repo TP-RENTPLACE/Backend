c014d068ac8cffadc0e54b3aa05455b52ffbf12d
Kattsyn <vov4ikkat3@mail.ru>
2025-05-14 01:21:03 +0300

    (TP-117) fix: add PATCH to allowedMethods

--- rentplace/src/main/java/kattsyn/dev/rentplace/configs/SecurityConfig.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/configs/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Set-Cookie"));
         config.setAllowCredentials(true);
