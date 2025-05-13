73c1cfe933d7570ab8d70e1473907e307ad68287
Kattsyn <vov4ikkat3@mail.ru>
2025-05-14 00:02:05 +0300

    (TP-117) feat: allowed all origins for dev stage

--- rentplace/src/main/java/kattsyn/dev/rentplace/configs/SecurityConfig.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/configs/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://rentplace.online", "http://admin.rentplace.online"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Set-Cookie"));
