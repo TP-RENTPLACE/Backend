784371b274ca54c8912b1663e39ff22cd02e71ab
Kattsyn <vov4ikkat3@mail.ru>
2025-05-16 23:24:14 +0300

    (TP-126) feat: add and impl getCodeResponse method

--- rentplace/src/main/java/kattsyn/dev/rentplace/services/AuthService.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/services/AuthService.java
@@ -1,13 +1,15 @@
 package kattsyn.dev.rentplace.services;
 
 import jakarta.security.auth.message.AuthException;
-import kattsyn.dev.rentplace.auth.JwtAuthentication;
 import kattsyn.dev.rentplace.dtos.requests.JwtRequest;
+import kattsyn.dev.rentplace.dtos.responses.CodeResponse;
 import kattsyn.dev.rentplace.dtos.responses.JwtResponse;
 import kattsyn.dev.rentplace.dtos.requests.RegisterRequest;
 import kattsyn.dev.rentplace.dtos.users.UserDTO;
 
 public interface AuthService {
+
+    CodeResponse getCodeResponse(String email);
 
     JwtResponse login(JwtRequest authRequest) throws AuthException;
 
@@ -19,9 +21,7 @@ public interface AuthService {
 
     JwtResponse refresh(String refreshToken) throws AuthException;
 
-    JwtAuthentication getAuthInfo();
-
     UserDTO getUserInfo() throws AuthException;
 
-    boolean validateCode(JwtRequest request) throws AuthException;
+    void validateCode(JwtRequest request) throws AuthException;
 }
