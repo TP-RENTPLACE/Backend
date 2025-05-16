efec3e1b0e04274ca81cf80f5d0f25f593cf9c1f
Kattsyn <vov4ikkat3@mail.ru>
2025-05-16 23:41:12 +0300

    (TP-126) refactor: delete extra test

--- rentplace/src/test/java/kattsyn/dev/rentplace/services/VerificationCodeServiceImplTest.java
+++ rentplace/src/test/java/kattsyn/dev/rentplace/services/VerificationCodeServiceImplTest.java
@@ -1,13 +1,11 @@
 package kattsyn.dev.rentplace.services;
 
-import kattsyn.dev.rentplace.enums.AuthType;
 import kattsyn.dev.rentplace.services.impl.VerificationCodeServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import kattsyn.dev.rentplace.dtos.responses.CodeResponse;
 import kattsyn.dev.rentplace.entities.VerificationCode;
 import kattsyn.dev.rentplace.exceptions.NotFoundException;
 import kattsyn.dev.rentplace.repositories.VerificationCodeRepository;
@@ -15,7 +13,6 @@ import kattsyn.dev.rentplace.repositories.VerificationCodeRepository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,12 +20,6 @@ class VerificationCodeServiceImplTest {
 
     @Mock
     private VerificationCodeRepository verificationCodeRepository;
-
-    @Mock
-    private EmailService emailService;
-
-    @Mock
-    private UserService userService;
 
     @InjectMocks
     private VerificationCodeServiceImpl verificationCodeService;
@@ -50,19 +41,6 @@ class VerificationCodeServiceImplTest {
         when(verificationCodeRepository.findById(email)).thenReturn(Optional.empty());
 
         assertThrows(NotFoundException.class, () -> verificationCodeService.validateCode(email, "12345"));
-    }
-
-    @Test
-    void generateAndSendCode_NewCodeGeneratedAndSaved() {
-        String email = "new@example.com";
-        when(userService.existsByEmail(email)).thenReturn(false);
-        when(verificationCodeRepository.findById(email)).thenReturn(Optional.empty());
-
-        CodeResponse response = verificationCodeService.generateAndSendCode(email);
-
-        verify(verificationCodeRepository).save(any(VerificationCode.class));
-        verify(emailService).sendVerificationCode(eq(email), anyString());
-        assertEquals(AuthType.AUTH_REGISTER, response.getAuthType());
     }
 
     @Test
