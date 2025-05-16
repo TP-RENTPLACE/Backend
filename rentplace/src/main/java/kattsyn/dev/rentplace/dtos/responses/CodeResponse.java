d0b1a319cee7a1c5cf4179381e9b63048ff6e19d
Kattsyn <vov4ikkat3@mail.ru>
2025-05-16 23:21:42 +0300

    (TP-126) feat: add userStatus to CodeResponse

--- rentplace/src/main/java/kattsyn/dev/rentplace/dtos/responses/CodeResponse.java
+++ rentplace/src/main/java/kattsyn/dev/rentplace/dtos/responses/CodeResponse.java
@@ -2,6 +2,7 @@ package kattsyn.dev.rentplace.dtos.responses;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import kattsyn.dev.rentplace.enums.AuthType;
+import kattsyn.dev.rentplace.enums.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +14,7 @@ public class CodeResponse {
 
     @Schema(description = "Тип аутентификации. AUTH_LOGIN, если пользователь существует, иначе AUTH_REGISTER")
     private AuthType authType;
+    @Schema(description = "Статус профиля пользователя. STATUS_BLOCKED, STATUS_ACTIVE, STATUS_INACTIVE")
+    private UserStatus userStatus;
 
 }
